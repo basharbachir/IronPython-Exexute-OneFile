ba8974dac210091b3bf3d568adbf15846ff2d581
basharbachir <bashar.bachir@gmail.com>
2018-10-06 23:07:08 +0200

    Add files via upload

---
+++ PythonMain.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Reflection;
+using IronPython.Runtime.Operations;
+namespace PythonApplication1
+{
+    public class PythonMain
+    {
+        [STAThread]
+        public static void Main()
+        {
+PythonOps.InitializeModuleEx(Assembly.Load(new byte[]{ /* put your dll bytes here*/ }), "__main__", null , false, null);    
+        }
+    }
+}
